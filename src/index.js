03cb01fa07d22f7b260b778de96cbfbee191db40
Derek Covey <derek.covey@gmail.com>
2018-07-17 15:19:28 -0700

    Connect Provider in index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(issApp, {});
+
 ReactDOM.render((
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" component={App} />
-        </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={App} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
 registerServiceWorker();
