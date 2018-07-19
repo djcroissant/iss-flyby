e283b2194dc49e18bcb8f256731008d9246b4271
Derek Covey <derek.covey@gmail.com>
2018-07-19 10:08:15 -0700

    Use thunk to allow action to return function

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -10,21 +10,45 @@ export const QUERY_API = 'QUERY_API';
 //     number: 100
 // }
 
+// export function queryApi(values) {
+//     return (dispatch) => {
+//         axios.get(
+//             "http://api.open-notify.org/iss-pass.json?lat=" +
+//             values.latitude.toString() +
+//             "&lon=" +
+//             values.longitude.toString() +
+//             "&n=" +
+//             values.number.toString()
+//         ).then(response => {
+//             let apiResponse = response.data.response;
+//             dispatch({
+//                 type: 'QUERY_API',
+//                 apiResponse
+//             });
+//         }).catch((err) => {
+//             console.log(err);
+//         });
+//     };
+// };
+
 export function queryApi(values) {
     return (dispatch) => {
-        axios.get(
+
+        const request = axios.get(
             "http://api.open-notify.org/iss-pass.json?lat=" +
             values.latitude.toString() +
             "&lon=" +
             values.longitude.toString() +
             "&n=" +
             values.number.toString()
-        ).then(response => {
+        )
+        
+        return request.then(response => {
             let apiResponse = response.data.response;
             dispatch({
                 type: 'QUERY_API',
                 apiResponse
-            });
+            })
         }).catch((err) => {
             console.log(err);
         });
