686e9185460a00c5178a8ad6e44ce809168ee90c
Derek Covey <derek.covey@gmail.com>
2018-07-27 11:47:03 -0700

    Create search button in Location component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Button = ({ text, handleClick }) => (
-  <button className="button middle calculate" onclick={handleClick}>
-    {text}
-  </button>
-);
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.props.handleClick.bind(this);
+  }
+
+  // handleClick() {
+  //   alert("hello!!!!")
+  // }
+
+  render() {
+    return (
+      <button className="button middle calculate" onClick={this.props.handleClick}>
+        {this.props.text}
+      </button>
+    );
+  }
+}
 
 export default Button
--- src/components/home_page/components/Location.js
+++ src/components/home_page/components/Location.js
@@ -3,28 +3,42 @@ import React from 'react';
 // Import images
 import circle_one from '../../../assets/images/circle_one.png';
 
-const Location = () => (
-    <div className="step-container">
-        <div className="step-component">
-            <img src={circle_one} className="circle_number" alt="1" />
-        </div>
-        <div className="step-component">
-            <h3>Enter your location</h3>
-            <label>
-                <input
-                    type="text"
-                />
-            </label>
-        </div>
-        <div className="step-component">
-            <h3>MAP</h3>
-            <h3>MAP</h3>
-            <h3>MAP</h3>
-            <h3>MAP</h3>
-            <h3>MAP</h3>
-            <h3>MAP</h3>
-        </div>
-    </div>
-)
+class Location extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    alert("why hello");
+  }
+
+  render() {
+    return (
+      <div className="step-container">
+          <div className="step-component">
+              <img src={circle_one} className="circle_number" alt="1" />
+          </div>
+          <div className="step-component">
+              <h3>Enter your location</h3>
+              <label>
+                  <input
+                      type="text"
+                  />
+              </label>
+              <button className="button middle calculate" onClick={this.handleClick}>Search</button>
+          </div>
+          <div className="step-component">
+              <h3>MAP</h3>
+              <h3>MAP</h3>
+              <h3>MAP</h3>
+              <h3>MAP</h3>
+              <h3>MAP</h3>
+              <h3>MAP</h3>
+          </div>
+      </div>
+    );
+  }
+}
 
 export default Location
